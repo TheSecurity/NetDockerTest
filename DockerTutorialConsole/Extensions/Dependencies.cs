60802f9a157ed26f3cfc5805e0fd20a3bf01a324
TheSecurity <jamiedesert01@gmail.com>
2022-03-17 07:19:15 +0100

    Adding application settings

---
+++ DockerTutorialConsole/Extensions/Dependencies.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DockerTutorialConsole.Extensions
+{
+    internal static class Dependencies
+    {
+        internal static IServiceCollection ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ DockerTutorialConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using DockerTutorialConsole.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json");
+    })
+    .ConfigureServices((context, services) =>
+    {
+        Dependencies.ConfigureServices(context.Configuration, services);
+    })
+    .Build();
+
+// Application code should start here.
+
+await host.RunAsync();
